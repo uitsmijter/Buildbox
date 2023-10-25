3580ecf5cd84b6cdd2420c51d8713866739829f5
Kris Simon <142403270+KrisSimon@users.noreply.github.com>
2023-10-25 20:00:57 +0200

    Version 1.1.0
    * Update to Swift 5.9.1
    * set version tag
    * Set ref name to release candidates
    * Use Slug for rc
    * print more information
    * Notify on Pull Requests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Uitsmijter"
+echo "Uitsmijter 1.1 Swift 5.9.1"
 echo "------------------------------------------------------------"
-
+echo "Visit: https://docs.uitsmijter.io"
+echo ""
 exec "$@"
+echo "------------------------------------------------------------"
