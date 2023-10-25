5b1ff82d6be4dbe260cefe2c3d2c6f0f7ef565b8
Kris Simon <kris@ausdertechnik.de>
2023-10-25 21:03:43 +0200

    Beautify console output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "Uitsmijter 1.1 Swift 5.9.1"
+echo ""
+echo "Uitsmijter Buildbox 1.1 | Swift 5.9.1"
 echo "------------------------------------------------------------"
 echo "Visit: https://docs.uitsmijter.io"
 echo ""
 exec "$@"
 echo "------------------------------------------------------------"
+echo ""
