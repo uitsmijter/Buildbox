5a77993e88b608e5ff5788a05188213003102d03
Kris Simon <kris@ausdertechnik.de>
2023-10-27 15:36:11 +0200

    Fix version information

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo ""
-echo "Uitsmijter Buildbox 1.1 | Swift 5.9.1"
+echo "Uitsmijter Buildbox 2.0.1 | Swift 5.9.1"
 echo "------------------------------------------------------------"
 echo "Visit: https://docs.uitsmijter.io"
 echo ""
