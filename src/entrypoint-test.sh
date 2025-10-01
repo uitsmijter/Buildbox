5c14c1b9276ead59deee8a3cfde8a3f8cbd2af33
Kris Simon <142403270+KrisSimon@users.noreply.github.com>
2025-10-01 14:08:32 +0200

    swift 5.10.1 (#7)
    * Swift 5.10.1
    
    * Update github-slug-action
    
    * use github-slug-action v4
    
    Prepare version: 2.4

--- src/entrypoint-test.sh
+++ src/entrypoint-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo ""
-echo "Uitsmijter TestRunner 2.3.0 | Swift 5.9.2"
+echo "Uitsmijter TestRunner 2.4.0 | Swift 5.10.1"
 echo "------------------------------------------------------------"
 echo "Visit: https://docs.uitsmijter.io"
 echo ""
